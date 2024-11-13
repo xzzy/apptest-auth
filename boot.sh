7b3a852da9481e683710cf15ab868cc067d436a8
Jason Moore <jmoore@austwa.com>
2024-11-13 17:31:10 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -3,18 +3,18 @@
 # Start the first process
 env > /etc/.cronenv
 
-service cron start &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start cron: $status"
-  exit $status
-fi
+# service cron start &
+# status=$?
+# if [ $status -ne 0 ]; then
+#   echo "Failed to start cron: $status"
+#   exit $status
+# fi
 
 # Start the second process
-service nginx start &
+nginx -c /app/nginx/conf/nginx.conf -p /app/nginx
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start apache2: $status"
+  echo "Failed to start nginx: $status"
   exit $status
 fi
 bash
